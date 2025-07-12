f66e62808bb846cda2bd06d6a210754d3dcce446
sananlb <sananlb@users.noreply.github.com>
2025-07-12 19:49:44 +0600

    Удаление дублирующейся структуры папок NDFL

--- NDFL/currency_CBRF/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'currency_CBRF'
-
-urlpatterns = [
-    # Здесь будут ваши URL-шаблоны
-]
--- NDFL/reports_to_ndfl/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ReportsToNdflConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'reports_to_ndfl'
